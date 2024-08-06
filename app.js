ab1b54297978e11027cc73996a6b24c1efd195a5
alireza-shokri <lshkry88@gmail.com>
2024-08-07 01:56:01 +0330

    create project

---
+++ app.js
@@ -0,0 +1,178 @@
+
+// list content
+
+let list_twolevel_flr_panel=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59']
+
+let list_twolevel_flr_nopanel=['C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++','C++']
+let list_twolevel_mdl_panel=['python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python','python']
+
+let list_twolevel_mdl_nopanel=['js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js','js']
+
+let list_twolevel_mdl=['node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js']
+
+let list_onelevel_panel=['html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html','html']
+let list_onelevel_notpanel=['style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style','style']
+let list_truelist=['freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom','freedom']
+
+
+// variabel
+let $=document;
+const kind1 = $.querySelector('#kind1');
+const kind2 = $.querySelector('#kind2');
+const kind3 = $.querySelector('#kind3');
+const flr = $.querySelector('#flr');
+const mdl = $.querySelector('#mdl');
+const panel = $.querySelector('#panel');
+const nopanel = $.querySelector('#nopanel');
+
+const buy1 = $.querySelector('#buy1');
+const buy2 = $.querySelector('#buy2');
+const buy3 = $.querySelector('#buy3');
+const buy4 = $.querySelector('#buy4');
+const buy5 = $.querySelector('#buy5');
+let which;
+let vaz;
+
+// all li
+const liTable = document.querySelectorAll('.variable');
+
+
+let btns=$.querySelectorAll('.btn')
+let btns2=$.querySelectorAll('.btn2')
+let btns3=$.querySelectorAll('.btn3')
+
+
+
+// clear_class
+function clear_special(vrodi){
+  vrodi.forEach(function(elm){
+       if(elm.classList.contains('special'))
+            elm.classList.remove('special')
+    })
+}
+
+// disabled false
+function disabled_false(vrodi2){
+    vrodi2.forEach(function(elm){
+        elm.disabled=true;
+})}
+
+// disabled true
+function disabled_true(vrodi2){
+    vrodi2.forEach(function(elm){
+        elm.disabled=false;
+})}
+
+
+// class_is
+function class_is(vrodi3){
+    for(var i=0;i<vrodi3.length;i++){
+        if(vrodi3[i].classList.contains('special'))
+            return true;
+    }
+    return false;
+}
+
+function btns_hed(event){
+     
+    if(event.target.id == 'kind1' && event.target !== which){
+        disabled_true(btns2)
+        disabled_true(btns3)
+        clear_special(btns)
+        event.target.classList.add('special')
+
+        if(!class_is(btns2)) btns2[0].classList.add('special')
+
+        if(!class_is(btns3)) btns3[0].classList.add('special')
+
+        
+    
+    }
+
+    if(event.target.id == 'kind2' && event.target !== which){
+        disabled_false(btns2)
+        clear_special(btns2)
+        disabled_true(btns3)
+        clear_special(btns)
+        event.target.classList.add('special')
+
+        if(!class_is(btns3)) btns3[0].classList.add('special')
+    }
+
+    if(event.target.id == 'kind3' && event.target !== which){
+        disabled_false(btns2)
+        disabled_false(btns3)
+        clear_special(btns2)
+        clear_special(btns3)
+        clear_special(btns)
+        event.target.classList.add('special')
+    }
+    
+    which=event.target;
+    write_table()
+}
+
+
+function btns2_hed(event){
+    clear_special(btns2)
+    event.target.classList.add('special')
+    write_table()
+}
+function btns3_hed(event){
+    clear_special(btns3)
+    event.target.classList.add('special')
+
+}
+
+// do write
+function do_write(which_list){
+    for(var i=0;i<liTable.length;i++){
+        liTable[i].innerHTML=(which_list[i])
+    }
+}
+
+// select list
+function write_table(){
+    if(btns[0].classList.contains('special')){
+        if(btns2[0].classList.contains('special')&& btns3[0].classList.contains('special'))
+            do_write(list_twolevel_flr_panel)
+
+        else if(btns2[0].classList.contains('special')&& btns3[1].classList.contains('special'))
+            do_write(list_twolevel_flr_nopanel)
+
+        else if(btns2[1].classList.contains('special')&& btns3[0].classList.contains('special'))
+              do_write(list_twolevel_mdl_panel)
+
+        else if(btns2[1].classList.contains('special')&& btns3[1].classList.contains('special'))
+            do_write(list_twolevel_mdl_nopanel)
+    }
+    else if(btns[1].classList.contains('special')){
+        if(btns3[0].classList.contains('special'))
+            do_write(list_onelevel_panel)
+        else if(btns3[1].classList.contains('special'))
+            do_write(list_onelevel_notpanel)
+    }
+
+}
+
+btns.forEach(function(btn){
+    btn.addEventListener('click',btns_hed)})
+
+    
+btns2.forEach(function(btn2){
+    btn2.addEventListener('click',btns2_hed)})
+
+btns3.forEach(function(btn3){
+    btn3.addEventListener('click',btns3_hed)})
+
+window.onload=function(){
+    for(var i=0;i<liTable.length;i++){
+        liTable[i].innerHTML=(list_twolevel_flr_panel[i])
+    }
+}
+
+
+
+
+
+ 
