b24ed9d333eb84a60ad2d59f087aaf10c6fbb9de
alireza-shokri <lshkry88@gmail.com>
2024-08-07 16:31:47 +0330

    add link && torbu

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-
 // list content
-
-let list_twolevel_flr_panel=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59']
-
-let list_twolevel_flr_nopanel=['C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++','C++']
-let list_twolevel_mdl_panel=['python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python','python']
-
-let list_twolevel_mdl_nopanel=['js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js','js']
-
-let list_twolevel_mdl=['node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js']
-
-let list_onelevel_panel=['html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html','html']
-let list_onelevel_notpanel=['style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style','style']
-let list_truelist=['freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom','freedom']
+let list_twolevel_flr_panel=[
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59'],
+    // link 
+    ['https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri']
+]
+let list_twolevel_flr_nopanel=[
+    ['C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++', 'C++','C++'] ,
+    // link 
+    ['https://en.wikipedia.org/wiki/Iran','https://en.wikipedia.org/wiki/Iran','https://en.wikipedia.org/wiki/Iran','https://en.wikipedia.org/wiki/Iran','https://en.wikipedia.org/wiki/Iran','https://en.wikipedia.org/wiki/Iran']
+]
+let list_twolevel_mdl_panel=[
+    ['python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python', 'python','python'],
+     // link 
+     ['https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri']
+]
+let list_twolevel_mdl_nopanel=[
+    ['js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js', 'js','js'],
+     // link 
+     ['https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri']
+]
+let list_twolevel_mdl=[
+    ['node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js', 'node.js'],
+     // link 
+     ['https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri']
+]
+let list_onelevel_panel=[
+    ['html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html', 'html','html'],
+     // link 
+     ['https://musicforprogramming.net/fiftyeight/','https://musicforprogramming.net/fiftyeight/','https://musicforprogramming.net/fiftyeight/','https://musicforprogramming.net/fiftyeight/','https://musicforprogramming.net/fiftyeight/','https://musicforprogramming.net/fiftyeight/']
+]
+let list_onelevel_notpanel=[
+    ['style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style', 'style','style'],
+     // link 
+     ['https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri']
+]
+let list_turbo=[
+    ['freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom', 'freedom','freedom'],
+     // link 
+     ['https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri','https://github.com/alireza-shokri']
+]
 
 
 // variabel
@@ -24,30 +50,22 @@ const flr = $.querySelector('#flr');
 const mdl = $.querySelector('#mdl');
 const panel = $.querySelector('#panel');
 const nopanel = $.querySelector('#nopanel');
-
-const buy1 = $.querySelector('#buy1');
-const buy2 = $.querySelector('#buy2');
-const buy3 = $.querySelector('#buy3');
-const buy4 = $.querySelector('#buy4');
-const buy5 = $.querySelector('#buy5');
+const buy = $.querySelectorAll('#buy');
 let which;
-let vaz;
 
 // all li
 const liTable = document.querySelectorAll('.variable');
-
 
 let btns=$.querySelectorAll('.btn')
 let btns2=$.querySelectorAll('.btn2')
 let btns3=$.querySelectorAll('.btn3')
 
 
-
 // clear_class
 function clear_special(vrodi){
   vrodi.forEach(function(elm){
-       if(elm.classList.contains('special'))
-            elm.classList.remove('special')
+    if(elm.status==true) elm.status=false;
+       if(elm.classList.contains('special')) elm.classList.remove('special');
     })
 }
 
@@ -63,7 +81,6 @@ function disabled_true(vrodi2){
         elm.disabled=false;
 })}
 
-
 // class_is
 function class_is(vrodi3){
     for(var i=0;i<vrodi3.length;i++){
@@ -74,106 +91,89 @@ function class_is(vrodi3){
 }
 
 function btns_hed(event){
-     
-    if(event.target.id == 'kind1' && event.target !== which){
-        disabled_true(btns2)
-        disabled_true(btns3)
-        clear_special(btns)
+    if(event.target.id == 'kind1' && event.target !== which) {
+        disabled_true(btns2);
+        disabled_true(btns3);
+        clear_special(btns);
         event.target.classList.add('special')
-
-        if(!class_is(btns2)) btns2[0].classList.add('special')
-
-        if(!class_is(btns3)) btns3[0].classList.add('special')
-
-        
-    
-    }
-
+        event.target.status=true;
+        if(!class_is(btns2)) {btns2[0].classList.add('special'),btns2[0].status=true} 
+        if(!class_is(btns3)) {btns3[0].classList.add('special') ,btns3[0].status=true} 
+    } 
     if(event.target.id == 'kind2' && event.target !== which){
-        disabled_false(btns2)
-        clear_special(btns2)
-        disabled_true(btns3)
-        clear_special(btns)
-        event.target.classList.add('special')
-
-        if(!class_is(btns3)) btns3[0].classList.add('special')
+        disabled_false(btns2);
+        clear_special(btns2);
+        disabled_true(btns3);
+        clear_special(btns);
+        event.target.classList.add('special');
+        event.target.status=true;
+        if(!class_is(btns3)){
+            btns3[0].classList.add('special');
+            btns3[0].status=true;
+        } 
     }
-
     if(event.target.id == 'kind3' && event.target !== which){
-        disabled_false(btns2)
-        disabled_false(btns3)
-        clear_special(btns2)
-        clear_special(btns3)
-        clear_special(btns)
-        event.target.classList.add('special')
+        disabled_false(btns2);
+        disabled_false(btns3);
+        clear_special(btns2);
+        clear_special(btns3);
+        clear_special(btns);
+        event.target.classList.add('special');
+        event.target.status=true;    
     }
-    
     which=event.target;
-    write_table()
+    write_table();
 }
-
 
 function btns2_hed(event){
-    clear_special(btns2)
-    event.target.classList.add('special')
-    write_table()
+    clear_special(btns2);
+    event.target.classList.add('special');
+    event.target.status=true;    
+    write_table();
 }
+
 function btns3_hed(event){
-    clear_special(btns3)
-    event.target.classList.add('special')
-    write_table()
+    clear_special(btns3);
+    event.target.classList.add('special');
+    event.target.status=true;    
+    write_table();
 }
 
 // do write
 function do_write(which_list){
-    for(var i=0;i<liTable.length;i++){
-        liTable[i].innerHTML=(which_list[i])
-    }
+    for(let i=0;i<liTable.length;i++){ liTable[i].innerHTML=(which_list[0][i])};
+    for(i=0;i<buy.length;i++){buy[i].setAttribute('href',(which_list[1][i]))};
 }
 
 // select list
 function write_table(){
-    if(btns[0].classList.contains('special')){
-        if(btns2[0].classList.contains('special')&& btns3[0].classList.contains('special'))
-            do_write(list_twolevel_flr_panel)
-
-        else if(btns2[0].classList.contains('special')&& btns3[1].classList.contains('special'))
-            do_write(list_twolevel_flr_nopanel)
-
-        else if(btns2[1].classList.contains('special')&& btns3[0].classList.contains('special'))
-              do_write(list_twolevel_mdl_panel)
-
-        else if(btns2[1].classList.contains('special')&& btns3[1].classList.contains('special'))
-            do_write(list_twolevel_mdl_nopanel)
-    }
-    else if(btns[1].classList.contains('special')){
-        if(btns3[0].classList.contains('special'))
-            do_write(list_onelevel_panel)
-        else if(btns3[1].classList.contains('special'))
-            do_write(list_onelevel_notpanel)
+    if(btns[0].status){
+        if(btns2[0].status && btns3[0].status) do_write(list_twolevel_flr_panel);
+        else if(btns2[0].status && btns3[1].status) do_write(list_twolevel_flr_nopanel);
+        else if(btns2[1].status && btns3[0].status)  do_write(list_twolevel_mdl_panel);
+        else if(btns2[1].status && btns3[1].status) do_write(list_twolevel_mdl_nopanel);
     }
 
-    
+    else if(btns[1].status){
+        if(btns3[0].status) do_write(list_onelevel_panel);
+        else if(btns3[1].status) do_write(list_onelevel_notpanel);
+    }
+
+    else if(btns[2].status) {do_write(list_turbo)}
 }
+
 
 btns.forEach(function(btn){
-    btn.addEventListener('click',btns_hed)})
-
-    
+    btn.addEventListener('click',btns_hed)});
 btns2.forEach(function(btn2){
-    btn2.addEventListener('click',btns2_hed)})
-
+    btn2.addEventListener('click',btns2_hed)});
 btns3.forEach(function(btn3){
-    btn3.addEventListener('click',btns3_hed)})
+    btn3.addEventListener('click',btns3_hed)});
+
 
 window.onload=function(){
-    for(var i=0;i<liTable.length;i++){
-        liTable[i].innerHTML=(list_twolevel_flr_panel[i])
-    }
+    do_write(list_twolevel_flr_panel);
+    btns[0].status=true;
+    btns2[0].status=true;
+    btns3[0].status=true;
 }
-
-
-
-
-
- 
